0b5f62b0f69d6f2d676790cb1cf305a96fd0f0cd
HP <madhankumarchavan26@gmail.com>
2024-01-06 19:38:47 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -4,8 +4,8 @@ script_location=$(pwd)
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
-useradd roboshop
-#mkdir /app
+#useradd roboshop
+mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
